968a41272974918248f3601e9a2acae37691cc8b
mburakaygul <mburakaygul@gmail.com>
2023-01-04 00:04:05 +0300

    Initial Commit

---
+++ wordlist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wordlist
+//
+//  Created by Burak Aygül on 4.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
